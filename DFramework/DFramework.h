f349a4666a7eb094f6ca591a794a7111c977c7e0
Yusuf Demirci <demirciy.software@gmail.com>
2020-11-21 12:46:31 +0100

    Initial Commit

---
+++ DFramework/DFramework.h
@@ -0,0 +1,18 @@
+//
+//  DFramework.h
+//  DFramework
+//
+//  Created by Yusuf Demirci on 21.11.20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DFramework.
+FOUNDATION_EXPORT double DFrameworkVersionNumber;
+
+//! Project version string for DFramework.
+FOUNDATION_EXPORT const unsigned char DFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DFramework/PublicHeader.h>
+
+
