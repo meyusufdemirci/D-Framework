3f3611327b6126ef0f8387a1cb582d550ef83b7e
Yusuf Demirci <demirciy.software@gmail.com>
2022-04-02 21:57:32 +0300

    Add alert and sheet container.

---
+++ Sources/DFramework/DAlertContainer.swift
@@ -0,0 +1,14 @@
+//
+//  DAlertContainer.swift
+//  
+//
+//  Created by Yusuf Demirci on 2.04.2022.
+//
+
+import SwiftUI
+
+public struct DAlertContainer: Identifiable {
+
+    public let id = UUID()
+    public let alert: Alert
+}
---
+++ Sources/DFramework/DSheetContainer.swift
@@ -0,0 +1,14 @@
+//
+//  DSheetContainer.swift
+//  
+//
+//  Created by Yusuf Demirci on 2.04.2022.
+//
+
+import SwiftUI
+
+public struct DSheetContainer: Identifiable {
+
+    public let id = UUID()
+    public let sheet: AnyView
+}
