bf3a999d6ee2b64ea646bde093f105bf27af0f89
Yusuf Demirci <demirciy.software@gmail.com>
2022-04-04 14:04:26 +0300

    Add: DImageFormat.

---
+++ Sources/DFramework/DImageFormat.swift
@@ -0,0 +1,14 @@
+//
+//  DImageFormat.swift
+//  
+//
+//  Created by Yusuf Demirci on 4.04.2022.
+//
+
+import Foundation
+
+public enum DImageFormat: String {
+    case png
+    case jpg
+    case jpeg
+}
